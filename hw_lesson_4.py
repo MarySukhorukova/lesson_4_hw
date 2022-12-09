553eaa970d061e4dafba36f0cdb6196f08ef88dc
MarySukhorukova <57411226+MarySukhorukova@users.noreply.github.com>
2022-12-09 07:15:33 +0300

    Update hw_lesson_4.py

--- hw_lesson_4.py
+++ hw_lesson_4.py
@@ -1,18 +1,18 @@
-def change_name(func, *args):
+def сorrecting_name(func, *args):
     name = func.__name__.title().replace('_', ' ')
     print(name, *args, sep=' - ')
 
 
 def open_browser(browser_name):
-    change_name(open_browser, browser_name)
+    сorrecting_name(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-    change_name(go_to_companyname_homepage, page_url)
+    сorrecting_name(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    change_name(find_registration_button_on_login_page, page_url, button_text)
+    сorrecting_name(find_registration_button_on_login_page, page_url, button_text)
 
 
 open_browser(browser_name="Chrome")
